d8f36bd9546e3361eaabd320aa778f5bc1eded0d
day-dd <68999681+day-dd@users.noreply.github.com>
2025-06-06 14:16:05 +0200

    Create revenue_by_product_line.sql

---
+++ motorcycle_part_sales_sql/revenue_by_product_line.sql
@@ -0,0 +1,35 @@
+-- -- (source: https://www.datacamp.com/projects/1574)
+-- The goal is to analyze sales data of a company that sells motorcycle parts.
+-- To-Dos:
+-- Calculate net revenue for each product line
+-- Group results by month and warehouse
+-- Filter the results by only "Wholesale" orders
+
+-- - Below is a description of the table "Sales" we are working with:
+-- Column	Data_type	Description
+-- order_number	VARCHAR	Unique order number.
+-- date	DATE	Date of the order, from June to August 2021.
+-- warehouse	VARCHAR	The warehouse that the order was made from— North, Central, or West.
+-- client_type	VARCHAR	Whether the order was Retail or Wholesale.
+-- product_line	VARCHAR	Type of product ordered.
+-- quantity	INT	Number of products ordered.
+-- unit_price	FLOAT	Price per product (dollars).
+-- total	FLOAT	Total price of the order (dollars).
+-- payment	VARCHAR	Payment method—Credit card, Transfer, or Cash.
+-- payment_fee	FLOAT	Percentage of total charged as a result of the payment method.
+
+
+-- --------------SQL queries--------------
+SELECT product_line,
+    CASE WHEN EXTRACT('month' from date) = 6 THEN 'June'
+        WHEN EXTRACT('month' from date) = 7 THEN 'July'
+        WHEN EXTRACT('month' from date) = 8 THEN 'August'
+    END as month,
+    warehouse,
+	SUM(total) - SUM(payment_fee) AS net_revenue
+FROM sales
+WHERE client_type = 'Wholesale'
+GROUP BY product_line, warehouse, month
+ORDER BY product_line, month, net_revenue DESC
+  
+-- See the results in query_results.csv
